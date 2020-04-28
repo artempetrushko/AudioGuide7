4d2064f5907590b84ab146611286616d0bd4313a
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2020-04-28 21:13:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/test/ui/attractions/AttractionAdapter.java
@@ -0,0 +1,82 @@
+package com.example.test.ui.attractions;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.test.AttractionActivity;
+import com.example.test.R;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class AttractionAdapter extends RecyclerView.Adapter<AttractionAdapter.ViewHolder> {
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+    public AttractionAdapter(Context ct, String[] s1, String[] s2, int img[]) {
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.attraction_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, AttractionActivity.class);
+                intent.putExtra("data1", data1[position]);
+                intent.putExtra("data2", data2[position]);
+                intent.putExtra("myImage", images[position]);
+                context.startActivity(intent);
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView myText1, myText2;
+        ImageView myImage;
+        ConstraintLayout mainLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.attraction_name);
+            myText2 = itemView.findViewById(R.id.attraction_address);
+            myImage = itemView.findViewById(R.id.attraction_image);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/test/ui/attractions/AttractionsFragment.java
@@ -0,0 +1,64 @@
+package com.example.test.ui.attractions;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.test.R;
+
+public class AttractionsFragment extends Fragment {
+
+    private AttractionsViewModel attractionsViewModel;
+    RecyclerView recyclerview;
+    String s1[];
+    String s2[];
+    int[] images = {R.drawable.keyboard_memorial, R.drawable.yeltsin_center, R.drawable.sevastyanov_house,
+                    R.drawable.temple_na_krovi, R.drawable.tatischev_memorial, R.drawable.ekaterinburg_circus,
+                    R.drawable.novotichvinskiy_monastery, R.drawable.black_tulip, R.drawable.rastorguev_manor,
+                    R.drawable.sverdlov_memorial};
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        /*attractionsViewModel =
+                ViewModelProviders.of(this).get(AttractionsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_attractions, container, false);
+        final TextView textView = root.findViewById(R.id.text_slideshow);
+        attractionsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+
+        /*recyclerview = recyclerview.findViewById(R.id.recyclerview);
+
+        s1 = getResources().getStringArray(R.array.attractions);
+        s1 = getResources().getStringArray(R.array.addresses);
+
+
+        recyclerview.setAdapter(attractionAdapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));*/
+
+        View view = inflater.inflate(R.layout.fragment_attractions, container, false);
+
+        recyclerview = (RecyclerView)view.findViewById(R.id.recyclerview);
+        s1 = getResources().getStringArray(R.array.attractions);
+        s2 = getResources().getStringArray(R.array.addresses);
+
+        AttractionAdapter attractionAdapter = new AttractionAdapter(getActivity(), s1, s2, images);
+
+        recyclerview.setAdapter(attractionAdapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+}
